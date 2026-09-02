0cefb05e9185ba35c194c437c7451a70aa3a9173
Aryan kumar  singh <f20230362@goa.bits-pilani.ac.in>
2026-09-02 16:57:38 +0530

    Time: 0 ms (100%), Space: 30.2 MB (94.95%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -1,21 +1,7 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        int n = nums1.size();
-       
-        //lets create all even
-        for(int i =0;i<n;i++){
-           if(nums1[i]%2!=0){
-            return true;
-           }
-
-        }
-        for(int i =0; i<n;i++){
-            if(nums1[i]%2==0){
-                return true;
-            }
-        }
-        return false;
+        return true;
         
     }
 };

8cb0b36c4af419926d1591bc3c4c16c5bad71012
Aryan kumar  singh <f20230362@goa.bits-pilani.ac.in>
2026-09-03 18:01:28 +0530

    Time: 0 ms (100%), Space: 165.9 MB (31.94%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int n = nums1.size();
+        int min = *min_element(nums1.begin(),nums1.end());
+
+        if(min%2==1){
+            return true;
+        }
+        for(int i =0;i<n;i++){
+            if(nums1[i]%2==1){
+                return false;
+            }
+        }
+        return true;
+    }
+};
